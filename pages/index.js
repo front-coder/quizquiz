bc899ea208deb4ad4213c59cb56f9a1e8e7987bc
front-coder <alex.mac.general@gmail.com>
2021-01-25 23:57:09 -0300

    Estrutura da primeira aula da Imersão React

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import { delBasePath } from 'next/dist/next-server/lib/router/router'
+import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+
+const QuizContainer = styled.div`
+	width: 100%;
+	max-width: 350px;
+	padding-top: 45px;
+	margin: auto 10%;
+	@media screen and (max-width: 500px) {
+		margin: auto;
+		padding: 15px;
+	}
+`;
+
+export default function Home() {
+	return (
+		<QuizBackground backgroundImage={db.bg}>
+			<QuizContainer>
+				<Widget>
+					<Widget.Header>
+						<h1>Texto do titulo</h1>						
+					</Widget.Header>
+					<Widget.Content>
+						<p>Texto corrido </p>
+					</Widget.Content>
+				</Widget>
+
+				<Widget>
+					<Widget.Content>
+						<h1>Tityulo outro bloco</h1>
+						<p>Texto do bloco </p>
+					</Widget.Content>
+				</Widget>
+				<Footer />
+			</QuizContainer>
+			<GitHubCorner projectUrl="https://github.com/front-coder" />
+		</QuizBackground>
+	)
+}
